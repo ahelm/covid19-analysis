84b41dc21d24482aa3d219805982cc9afff98202
Anton Helm <anton.helm@tecnico.ulisboa.pt>
2021-02-02 20:07:17 +0000

    remove update data script -> moved to notebook

--- update_data.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-data_source = (
-    "https://raw.githubusercontent.com/"
-    "owid/covid-19-data/master/public/data/owid-covid-data.csv"
-)
-
-r = requests.get(data_source, allow_redirects=True)
-open("covid-data.csv", "wb").write(r.content)
